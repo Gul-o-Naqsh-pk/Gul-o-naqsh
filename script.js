56ef2931be491731add0f3cffc9f2a6e95537a7b
Gul-o-Naqsh <intraperneur@gmail.com>
2026-07-10 18:29:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -38,5 +38,33 @@ function logout() {
 if (window.location.pathname.includes("admin.html")) {
   if (localStorage.getItem("adminLoggedIn") !== "true") {
     window.location.href = "admin-login.html";
+      function adminLogin(event) {
+event.preventDefault();
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+const message = document.getElementById("message");
+
+if (username === "admin" && password === "12345") {
+localStorage.setItem("adminLoggedIn", "true");
+window.location.href = "admin.html";
+} else {
+message.innerHTML = "Wrong username or password";
+}
+}
+
+function logout() {
+localStorage.removeItem("adminLoggedIn");
+window.location.href = "admin-login.html";
+}
+
+if (window.location.pathname.includes("admin.html")) {
+const loggedIn = localStorage.getItem("adminLoggedIn");
+
+if (loggedIn !== "true") {
+window.location.href = "admin-login.html";
+}
+}
+
   }
 }
