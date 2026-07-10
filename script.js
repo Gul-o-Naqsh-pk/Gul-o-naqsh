42970aa06e1959bfdb590076539212622ef6f6a7
Gul-o-Naqsh <intraperneur@gmail.com>
2026-07-10 18:24:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ document.querySelectorAll("nav a").forEach(link => {
 });
 
 console.log("Welcome to Gul-o-Nasque!");
+function adminLogin(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "12345") {
+    localStorage.setItem("adminLoggedIn", "true");
+    window.location.href = "admin.html";
+  } else {
+    document.getElementById("message").innerText =
+      "Wrong username or password.";
+  }
+}
+
+function logout() {
+  localStorage.removeItem("adminLoggedIn");
+  window.location.href = "admin-login.html";
+}
+
+if (window.location.pathname.includes("admin.html")) {
+  if (localStorage.getItem("adminLoggedIn") !== "true") {
+    window.location.href = "admin-login.html";
+  }
+}
